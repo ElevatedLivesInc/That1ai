1f8bfa14a4c0a4f0dede90fa7ed4ff17ae50bcf1
ElevatedLivesInc <gabriel@elevatedlivesinc.com>
2025-12-27 11:58:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Simple script just to set the year
 document.addEventListener("DOMContentLoaded", () => {
   const el = document.getElementById("year");
   if (el) el.textContent = new Date().getFullYear();
